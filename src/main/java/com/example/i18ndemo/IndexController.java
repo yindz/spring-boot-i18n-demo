cc70cf565cbc71eb56c73e4e0d0ef1f22ab4c9cf
yinzl <yinzl@cfzq.com>
2019-11-08 10:03:02 +0800

    完善案例

--- src/main/java/com/example/i18ndemo/IndexController.java
+++ src/main/java/com/example/i18ndemo/IndexController.java
@@ -1,7 +1,13 @@
 package com.example.i18ndemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Locale;
 
 /**
  * 演示接口
@@ -9,8 +15,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping(value = "/index")
-    public String index(){
-        return "index";
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping(value = "/")
+    public String index(ModelMap model){
+        Locale locale = LocaleContextHolder.getLocale();
+        //手动取值
+        model.addAttribute("copyrightMsg", messageSource.getMessage("system.copyright", null, locale));
+        return "home";
     }
 }
